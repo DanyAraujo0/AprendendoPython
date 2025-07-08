b65706f5997e893ec0ae47d29f38c068c059f3ba
DanyAraujo <araujodanyelle48@gmail.com>
2025-07-08 18:47:34 -0300

    poo

---
+++ POO/heranca_conceitos.py
@@ -0,0 +1,8 @@
+class Pai: # pai ou seja a herança será passada as outras classes
+    pass
+
+class Filha(Pai): # recebe a herança de pai - herança simples
+    pass
+
+class Neta(Pai,Filha): # recebe de pai e filha a herança - herança múltipla
+    pass
