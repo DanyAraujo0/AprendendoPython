866e7de782f219f620deb473bb7298f8481c4d61
DanyAraujo <araujodanyelle48@gmail.com>
2025-07-21 19:03:32 -0300

    1436 m

--- desenvolvimento_web/workout_api/atleta/models.py
+++ desenvolvimento_web/workout_api/atleta/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Datetime,Integer, String, Float, ForeignKey
+from sqlalchemy import DateTime,Integer, String, Float, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
 
@@ -8,13 +8,13 @@ class AtletaModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer,primary_key=True)
     nome: Mapped[str] = mapped_column(String[50],nullable=False)
-    cpf: Mapped[str] = mapped_column(String[11],nullable=False)
+    cpf: Mapped[str] = mapped_column(String[11],unique=True,nullable=False)
     idade: Mapped[int] = mapped_column(Integer,nullable=False)
     peso: Mapped[float] = mapped_column(Float,nullable=False)
     altura: Mapped[float] = mapped_column(Float,nullable=False)
     sexo: Mapped[str] = mapped_column(String[1],nullable=False)
-    created_at: Mapped[datetime] = mapped_column(Datetime,nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime,nullable=False)
     categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
     categoria_id: Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
     centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates='atleta')
-    centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey('centro_treinamento.pk_id'))
+    centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey('centros_treinamento.pk_id'))
--- desenvolvimento_web/workout_api/categorias/models.py
+++ desenvolvimento_web/workout_api/categorias/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Datetime,Integer, String
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
 
@@ -6,5 +6,5 @@ class CategoriaModel(BaseModel):
     __tablename__ = 'categorias'
 
     pk_id: Mapped[int] = mapped_column(Integer,primary_key=True)
-    nome: Mapped[str] = mapped_column(String[50],nullable=False)
+    nome: Mapped[str] = mapped_column(String[50],unique=True,nullable=False)
     atleta: Mapped['AtletaModel'] = relationship(back_populates='categoria')
